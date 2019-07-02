b0addc1a41b6db0d125da155a2a6ad965e0baee3
Bob Paauwe <bpaauwe@bobsplace.com>
2019-07-02 16:02:02 -0700

    Add more status information

--- mower.py
+++ mower.py
@@ -76,7 +76,7 @@ class Controller(polyinterface.Controller):
         for node in self.nodes:
             if self.nodes[node].id == 'mower':
                 LOGGER.info('Calling node get_status')
-                self.nodes[node].get_status()
+                self.nodes[node].get_status(False)
             else:
                 LOGGER.info('Not getting status for node with id = ' + self.nodes[node].id)
 
@@ -106,7 +106,7 @@ class Controller(polyinterface.Controller):
             mower_node.internal_id = '100-1' 
 
         self.addNode(mower_node)
-        self.nodes['automow'].get_status()
+        self.nodes['automow'].get_status(True)
             
 
     # Delete the node server from Polyglot
@@ -163,28 +163,80 @@ class mowerNode(polyinterface.Node):
     mower = None
     drivers = [
             {'driver': 'ST', 'value': 0, 'uom': 2},   # status
-            {'driver': 'GV0', 'value': 0, 'uom': 25},   # battery
+            {'driver': 'GV0', 'value': 0, 'uom': 51},   # battery
             {'driver': 'GV1', 'value': 0, 'uom': 25},   # operating mode
             {'driver': 'GV2', 'value': 0, 'uom': 25},   # last error
             {'driver': 'GV3', 'value': 0, 'uom': 25},   # start source
             {'driver': 'GV4', 'value': 0, 'uom': 56},   # start timestamp
+            {'driver': 'GV5', 'value': 0, 'uom': 25},   # status mode
+            {'driver': 'GV6', 'value': 0, 'uom': 25},   # status activity
+            {'driver': 'GV7', 'value': 0, 'uom': 25},   # status state
+            {'driver': 'GV8', 'value': 0, 'uom': 25},   # reason
+            {'driver': 'GV9', 'value': 0, 'uom': 25},   # status type
             ]
 
     def operating_modes(self, modename):
         if modename == 'AUTO':
             return 0
+        elif modename == 'HOME':
+            return 1
         else:
             LOGGER.info('Unknown next start source: ' + modename)
-            return 1
+            return 2
 
     def source(self, sourcename):
         if sourcename == 'COUNTDOWN_TIMER':
             return 0
+        elif sourcename == 'MOWER_CHARGING':
+            return 1
+        elif sourcename == 'WEEK_TIMER':
+            return 2
         else:
-            LOGGER.info('Unknown next start source: ' + sourcename)
+            return 3
+
+    def st_mode(self, name):
+        if name == 'HOME':
+            return 0
+        else:
+            return 1
+        
+    def st_activity(self, name):
+        if name == 'PARKED_IN_CS':
+            return 1
+        elif name == 'LEAVING':
+            return 2
+        elif name == 'MOWING':
+            return 3
+        elif name == 'GOING_HOME':
+            return 4
+        else:
+            return 5
+
+    def st_state(self, name):
+        if name == 'IN_OPERATION':
+            return 0
+        elif name == 'PAUSED':
+            return 1
+        elif name == 'RESTRICTED':
+            return 2
+        else:
+            LOGGER.info('Unknown mower state: ' + name)
+            
+    def st_reason(self, name):
+        if name == 'PARK_OVERRID':
+            return 0
+        else:
             return 1
 
-    def get_status(self):
+    def st_type(self, name):
+        if name == 'WEEK_SCHEDULE':
+            return 0
+        elif name == 'OVERRIDE':
+            return 1
+        else:
+            return 2
+
+    def get_status(self, first):
         try:
             json = self.mower.query('status')
             LOGGER.info(self.json)
@@ -195,21 +247,36 @@ class mowerNode(polyinterface.Node):
             last_error = json['lastErrorCode']
             start_source = source(json['nextStartSource'])
             start_timestamp = json['nextStartTimestamp']
+            st_mode = st_mode(json['mowerStatus']['mode'])
+            st_activity = st_activity(json['mowerStatus']['activity'])
+            st_state = st_state(json['mowerStatus']['state'])
+            st_reason = st_reason(json['mowerStatus']['restrictedReason'])
+            st_type = st_type(json['mowerStatus']['type'])
 
-            self.setDriver('ST', status, report=True, force=False)
-            self.setDriver('GV0', battery, report=True, force=False)
-            self.setDriver('GV1', mode, report=True, force=False)
-            self.setDriver('GV2', last_error, report=True, force=False)
-            self.setDriver('GV3', start_source, report=True, force=False)
-            self.setDriver('GV4', start_timestamp, report=True, force=False)
+            self.setDriver('ST', status, report=True, force=first)
+            self.setDriver('GV0', battery, report=True, force=first)
+            self.setDriver('GV1', mode, report=True, force=first)
+            self.setDriver('GV2', last_error, report=True, force=first)
+            self.setDriver('GV3', start_source, report=True, force=first)
+            self.setDriver('GV4', start_timestamp, report=True, force=first)
+            self.setDriver('GV5', st_mode, report=True, force=first)
+            self.setDriver('GV6', st_activity, report=True, force=first)
+            self.setDriver('GV7', st_state, report=True, force=first)
+            self.setDriver('GV8', st_reason, report=True, force=first)
+            self.setDriver('GV9', st_type, report=True, force=first)
 
         except:
-            self.setDriver('ST', 1, report=True, force=False)
-            self.setDriver('GV0', 100, report=True, force=False)
-            self.setDriver('GV1', 0, report=True, force=True)
-            self.setDriver('GV2', 0, report=True, force=False)
-            self.setDriver('GV3', 1, report=True, force=True)
-            self.setDriver('GV4', 565656565, report=True, force=False)
+            self.setDriver('ST', 1, report=True, force=first)
+            self.setDriver('GV0', 100, report=True, force=first)
+            self.setDriver('GV1', 0, report=True, force=first)
+            self.setDriver('GV2', 0, report=True, force=first)
+            self.setDriver('GV3', 1, report=True, force=first)
+            self.setDriver('GV4', 565656565, report=True, force=first)
+            self.setDriver('GV5', 1, report=True, force=first)
+            self.setDriver('GV6', 3, report=True, force=first)
+            self.setDriver('GV7', 0, report=True, force=first)
+            self.setDriver('GV8', 0, report=True, force=first)
+            self.setDriver('GV9', 1, report=True, force=first)
             LOGGER.debug('Skipping status, no connection to mower')
 
         # TODO: What does the response look like?  We need to translate
